244ce7c2d35f70947b9c836c4d5e4b121bf43110
Savio Shaji <savioshaji02@gmail.com>
2024-08-18 23:08:27 +0530

    save progress on hero section

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+
+const Hero = () => {
+  return (
+    <header className="bg-slate-100 text-slate-800 font-inter h-screen flex flex-col justify-center items-center px-4 text-center">
+     
+      <h1 className="text-3xl md:text-4xl lg:text-4xl font-bold mb-4">
+       Generate a recipe , quick and easy
+      </h1>
+
+      
+     
+    </header>
+  );
+};
+
+export default Hero;
