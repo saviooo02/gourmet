83b18cd867f611cf171b4a3f378132c3fedb7ad8
Savio Shaji <savioshaji02@gmail.com>
2024-08-19 10:56:19 +0530

    Add Hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,14 +1,17 @@
+import React from "react";
 
 const Hero = () => {
   return (
-    <header className="bg-slate-100 text-slate-800 font-inter h-screen flex flex-col justify-center items-center px-4 text-center">
-     
-      <h1 className="text-3xl md:text-4xl lg:text-4xl font-bold mb-4">
-       Generate a recipe , quick and easy
-      </h1>
+    <header className="bg-slate-100 text-slate-600 font-inter h-screen flex flex-col justify-center items-center px-4 text-center">
+      <div className="absolute top-36 w-full max-w-2xl">
+        <h1 className="text-2xl md:text-3xl lg:text-3xl font-bold mb-4">
+          Gourmet is an AI-Powered Recipe Generator
+        </h1>
 
-      
-     
+        <p className="text-base text-slate-400 font-inter md:text-lg lg:text-lg">
+          Simply input your preferences, and get creative, personalized recipes in seconds!
+        </p>
+      </div>
     </header>
   );
 };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className="bg-slate-100 text-slate-800 font-inter px-6 py-4 flex justify-end items-center">
+    <nav className="bg-slate-100 text-slate-600 font-inter px-6 py-4 flex justify-end items-center">
       <div className="flex space-x-11">
         <a
           href="https://github.com/your-username"
           target="_blank"
           rel="noopener noreferrer"
-          className="hover:text-slate-600"
+          className="hover:text-slate-400"
         >
           <FaGithub size={24} />
         </a>
@@ -17,7 +17,7 @@ const Navbar = () => {
           href="https://linkedin.com/in/your-username"
           target="_blank"
           rel="noopener noreferrer"
-          className="hover:text-slate-600"
+          className="hover:text-slate-400"
         >
           <FaLinkedin size={24} />
         </a>
