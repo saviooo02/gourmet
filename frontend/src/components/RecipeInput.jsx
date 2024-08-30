0771128fc895caaaacee4b0a3aa3c6206511a190
Savio Shaji <savioshaji02@gmail.com>
2024-08-31 01:01:45 +0530

    edit

--- frontend/src/components/RecipeInput.jsx
+++ frontend/src/components/RecipeInput.jsx
@@ -7,7 +7,7 @@ const RecipeInput = ({ setRecipe }) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('https://gourmet-server.vercel.app', {
+            const response = await fetch('https://gourmet-server.vercel.app/recipe', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
